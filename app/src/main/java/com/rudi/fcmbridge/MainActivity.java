dfda2aa0398f3b48e3fe9d0e9776676cd1947466
denikhuma1-ship-it <denikhuma1@gmail.com>
2025-09-14 00:37:02 +0700

    Create MainActivity.java

---
+++ app/src/main/java/com/rudi/fcmbridge/MainActivity.java
@@ -0,0 +1,12 @@
+package com.rudi.fcmbridge;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Kosong saja, biar app bisa di-install
+    }
+}
